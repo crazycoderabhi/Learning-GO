bf9427da400983bbf5acebcbc2876dce86863b82
crazycoderabhi <43914661+crazycoderabhi@users.noreply.github.com>
2019-03-05 02:04:10 +0530

    Created a simple Hello World GO application

---
+++ Go_Start/Go_Start.go
@@ -0,0 +1,10 @@
+/*First Line of our GO code is usually the name of the package we require */
+package main
+
+// Import used for importing different packages, here fmt is used for input and output operations
+import "fmt"
+
+// Defining a normal function called main, which just prints a hello world.
+func main() {
+	fmt.Println("hello, world")
+}
